943998cf83ee7c3eecd6602703301d07e91091ba
shuuky <shuucream2011@gmail.com>
2018-09-04 19:34:41 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -42,6 +42,7 @@ class FaceDetector(object):
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
 
         # 顔の数を表示する
+        img = np.zeros((512,512,3), np.uint8)
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(img,len(faces),(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
 
